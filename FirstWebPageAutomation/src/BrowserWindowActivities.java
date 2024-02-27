d735d34052690cf6fb14868e44bfd319c7ed64b2
Dell <Dell@DESKTOP-4M2K8CD>
2024-02-28 00:11:25 +0530

    Initial Commit.

---
+++ FirstWebPageAutomation/src/BrowserWindowActivities.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrowserWindowActivities {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver =new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		driver.navigate().to("https://rahulshettyacademy.com/locatorspractice/");
+		driver.navigate().back();
+		driver.navigate().forward();
+	}
+
+}
