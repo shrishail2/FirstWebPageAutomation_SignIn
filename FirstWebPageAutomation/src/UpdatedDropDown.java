b6ae2a3917bae2372a6aa1d0b6e21f59dab61034
Dell <Dell@DESKTOP-4M2K8CD>
2024-03-01 16:25:46 +0530

    Initial Commit.

---
+++ FirstWebPageAutomation/src/UpdatedDropDown.java
@@ -0,0 +1,39 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class UpdatedDropDown {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriver driver =new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+        driver.findElement(By.id("divpaxinfo")).click();
+        Thread.sleep(1000);
+        //driver.findElement(By.id("hrefIncAdt")).;
+        //by using for loop
+      /*  for (int i =1 ;i <=3;i++)
+        {
+        	driver.findElement(By.id("hrefIncAdt")).click();
+          
+        	
+       
+        }*/
+      //by using while loop
+        int i =1;
+        while(i<5) 
+        {
+        	driver.findElement(By.id("hrefIncAdt")).click();
+        	i++;
+        }
+        driver.findElement(By.id("btnclosepaxoption")).click();
+        System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+      //*[@id = 'divpaxinfo']
+      //  Thread.sleep(1000);
+        //driver.findElement(By.xpath("//*[@id = 'divpaxinfo']")).getText();
+
+       
+		 
+	}
+
+}
